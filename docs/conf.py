ddabd298688a32e90111d61d61eb38092de97089
liuxinwei <liuxinwei@xmsilicon.cn>
2022-05-31 12:52:55 +0800

    init

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,9 @@ version = loc["__version__"]
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "zh_CN"
+locale_dirs = ['../locales/']  # path is example but recommended.
+gettext_compact = False  # optional.
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -125,24 +127,24 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+# on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
+# if not on_rtd:  # only import and set the theme if we're building docs locally
+#     import sphinx_rtd_theme
 
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#     html_theme = "sphinx_rtd_theme"
+#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-    html_context = {"css_files": ["_static/theme_overrides.css"]}
-else:
-    html_context = {
-        "css_files": [
-            "//media.readthedocs.org/css/sphinx_rtd_theme.css",
-            "//media.readthedocs.org/css/readthedocs-doc-embed.css",
-            "_static/theme_overrides.css",
-        ]
-    }
-
+#     html_context = {"css_files": ["_static/theme_overrides.css"]}
+# else:
+#     html_context = {
+#         "css_files": [
+#             "//media.readthedocs.org/css/sphinx_rtd_theme.css",
+#             "//media.readthedocs.org/css/readthedocs-doc-embed.css",
+#             "_static/theme_overrides.css",
+#         ]
+#     }
+html_theme = 'sphinx_book_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
